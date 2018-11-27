388e4a588dd0141fd4c5e6254d3e1935f34f201e
唐卓 <tangz@yomob.com>
2018-11-27 19:27:19 +0800

    接入tenjin

---
+++ Assets/Scripts/TenjinManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TenjinManager : MonoBehaviour {
+    BaseTenjin instance;
+
+    void Start () {
+        TenjinInit ();
+    }
+
+    void OnApplicationPause(bool pauseStatus){
+        if (pauseStatus) {
+            //do nothing
+        }
+        else {
+            TenjinInit ();
+        }
+    }
+
+    void TenjinInit(){
+        instance = Tenjin.getInstance("SRKZX22EFVNAVDQJ5QZC5WJVWNMWLQ2N");
+        instance.Connect();
+    }
+}
