38e5eb2a111ede7f0bb26e97b98efc649aeb5e52
唐卓 <tangz@yomob.com>
2018-12-21 21:32:51 +0800

    修复引导适配

--- Assets/Scripts/ChristmasGuide.cs
+++ Assets/Scripts/ChristmasGuide.cs
@@ -10,6 +10,8 @@ public class ChristmasGuide : MonoBehaviour {
 
 	public GameObject vip;
 	public GameObject noads;
+
+	public Transform mapBtn;
 	// Use this for initialization
 	void Start () {
 		StartGuide ();
@@ -24,6 +26,7 @@ public class ChristmasGuide : MonoBehaviour {
 				if (!vip.activeSelf && !noads.activeSelf) {					
 					guideMask.SetActive (true);
 					map.SetActive (true);
+					map.transform.position = mapBtn.position;
 					map.GetComponent<Button> ().onClick.AddListener (() => {
 						map.SetActive(false);
 						map4.SetActive(true);
@@ -36,4 +39,5 @@ public class ChristmasGuide : MonoBehaviour {
 			}
 		}
 	}
+		
 }
